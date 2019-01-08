d41436724bb0877b2398a4b20629e86e910d0399
Darcy Cowan <dwc1@students.waikato.ac.nz>
2019-01-09 11:48:16 +1300

    committing ex properties

--- src/ictgradschool/web/lab13/ex1/Exercise01.java
+++ src/ictgradschool/web/lab13/ex1/Exercise01.java
@@ -2,14 +2,15 @@ package ictgradschool.web.lab13.ex1;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Exercise01 {
     public static void main(String[] args) {
         /* The following verifies that your JDBC driver is functioning. You may base your solution on this code */
+        Scanner sc = new Scanner(System.in);
+        String title;
         Properties dbProps = new Properties();
 
         try(FileInputStream fIn = new FileInputStream("mysql.properties")) {
@@ -22,6 +23,31 @@ public class Exercise01 {
         try (Connection conn = DriverManager.getConnection(dbProps.getProperty("url"), dbProps)) {
             System.out.println("Connection successful");
             //Anything that requires the use of the connection should be in here...
+            //setting up prepared statement
+            System.out.println("what title do you want to search for?: ");
+            title = sc.nextLine();
+
+            try(PreparedStatement pStmt= conn.prepareStatement(
+                    "SELECT * FROM lab13_articles WHERE title LIKE '%'?'%';")){
+                pStmt.setString(1,title);
+
+
+            try (ResultSet rs = pStmt.executeQuery()) {
+                int titleCol=rs.findColumn("title");
+                int bodyCol = rs.findColumn("body");
+
+                while (rs.next()){
+                    String retTitle = rs.getString(titleCol);
+                    String retBody = rs.getString(bodyCol);
+
+                    System.out.println(retTitle+"\n\n"+retBody);
+                }
+
+            }
+        }
+
+
+
 
 
 
