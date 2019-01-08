94c82e952f8cbc37d68df70e249832ca6a26eac4
Darcy Cowan <dwc1@students.waikato.ac.nz>
2019-01-09 12:04:44 +1300

    fin ex01

--- src/ictgradschool/web/lab13/ex1/Exercise01.java
+++ src/ictgradschool/web/lab13/ex1/Exercise01.java
@@ -10,7 +10,7 @@ public class Exercise01 {
     public static void main(String[] args) {
         /* The following verifies that your JDBC driver is functioning. You may base your solution on this code */
         Scanner sc = new Scanner(System.in);
-        String title;
+        String title="";
         Properties dbProps = new Properties();
 
         try(FileInputStream fIn = new FileInputStream("mysql.properties")) {
@@ -24,28 +24,31 @@ public class Exercise01 {
             System.out.println("Connection successful");
             //Anything that requires the use of the connection should be in here...
             //setting up prepared statement
-            System.out.println("what title do you want to search for?: ");
-            title = sc.nextLine();
+            while (title.length()==0) {
+                System.out.println("what title do you want to search for?: ");
+                title = sc.nextLine();
 
-            try(PreparedStatement pStmt= conn.prepareStatement(
-                    "SELECT * FROM lab13_articles WHERE title LIKE ?;")){
-                pStmt.setString(1,title);
+                try (PreparedStatement pStmt = conn.prepareStatement(
+                        "SELECT * FROM lab13_articles WHERE title LIKE ?;")) {
+                    pStmt.setString(1, "%" + title + "%");
 
 
-            try (ResultSet rs = pStmt.executeQuery()) {
-                int titleCol=rs.findColumn("title");
-                int bodyCol = rs.findColumn("body");
+                    try (ResultSet rs = pStmt.executeQuery()) {
+                        int titleCol = rs.findColumn("title");
+                        int bodyCol = rs.findColumn("body");
 
-                while (rs.next()){
-                    String retTitle = rs.getString(titleCol);
-                    String retBody = rs.getString(bodyCol);
+                        while (rs.next()) {
+                            String retTitle = rs.getString(titleCol);
+                            String retBody = rs.getString(bodyCol);
 
-                    System.out.println(retTitle+"\n\n"+retBody);
+                            System.out.println(retTitle + "\n\n" + retBody + "\n\n");
+                        }
+
+                    }
+
                 }
-
+                title="";
             }
-        }
-
 
 
 
