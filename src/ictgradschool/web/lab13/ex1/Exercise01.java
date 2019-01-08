69a37754eaa1084acec5697e089ba44d45e34b63
Darcy Cowan <dwc1@students.waikato.ac.nz>
2019-01-09 11:58:07 +1300

    I deleted the thing

--- src/ictgradschool/web/lab13/ex1/Exercise01.java
+++ src/ictgradschool/web/lab13/ex1/Exercise01.java
@@ -28,7 +28,7 @@ public class Exercise01 {
             title = sc.nextLine();
 
             try(PreparedStatement pStmt= conn.prepareStatement(
-                    "SELECT * FROM lab13_articles WHERE title LIKE '%'?'%';")){
+                    "SELECT * FROM lab13_articles WHERE title LIKE ?;")){
                 pStmt.setString(1,title);
 
 
