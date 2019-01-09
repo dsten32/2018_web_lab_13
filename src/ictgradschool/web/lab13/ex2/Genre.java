2bedac60e394e48508c2e6c99fd8462b4219e51d
Darcy Cowan <dwc1@students.waikato.ac.nz>
2019-01-09 16:16:39 +1300

    making classes more, more

--- src/ictgradschool/web/lab13/ex2/Genre.java
+++ src/ictgradschool/web/lab13/ex2/Genre.java
@@ -1,4 +1,27 @@
 package ictgradschool.web.lab13.ex2;
 
 public class Genre {
+    private int id;
+    private String genre;
+
+    public Genre(int id, String genre) {
+        this.id = id;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
