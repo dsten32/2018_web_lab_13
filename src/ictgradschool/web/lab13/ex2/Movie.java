3be9c7860e496ff78d19d92ef8e2ad21bc2f6e22
Darcy Cowan <dwc1@students.waikato.ac.nz>
2019-01-09 16:13:27 +1300

    making classes more

--- src/ictgradschool/web/lab13/ex2/Movie.java
+++ src/ictgradschool/web/lab13/ex2/Movie.java
@@ -1,4 +1,36 @@
 package ictgradschool.web.lab13.ex2;
 
 public class Movie {
+    private int id;
+    private String title,genre;
+
+    public Movie(int id, String title, String genre){
+        this.id = id;
+        this.genre = genre;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
